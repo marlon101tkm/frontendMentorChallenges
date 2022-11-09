ef94c181c57024a00173a24622d1e938a1155db2
Marlon <marlonmousquer910@gmail.com>
2022-11-09 18:08:37 -0300

    new project

---
+++ faq-accordion-card-main/js/index.js
@@ -0,0 +1,26 @@
+let answers = document.getElementsByClassName("answer");
+let arrows = document.getElementsByClassName("icon");
+
+
+
+function showAnswer(){
+    let classList = this.classList;
+    let answer = this.parentElement.nextElementSibling;
+    let question = this.parentElement.children[0];
+
+    if(classList.contains("icon-up")){
+        console.log("verdade");
+        classList.remove("icon-up");
+        answer.classList.remove("open");
+        question.classList.remove("bold");
+    }else{
+        classList.add("icon-up");
+        answer.classList.add("open");
+        question.classList.add("bold");
+    }
+}
+
+
+for (let arrow of arrows){
+    arrow.addEventListener("click",showAnswer)
+}
